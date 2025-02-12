93a592a331794e1d96759e4c32f7c49c95f0b8f6
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-02-12 23:52:11 +0530

    ip address added

--- shoe_spectra/shoe_spectra/settings.py
+++ shoe_spectra/shoe_spectra/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.201.23.100']
 
 
 # Application definition
