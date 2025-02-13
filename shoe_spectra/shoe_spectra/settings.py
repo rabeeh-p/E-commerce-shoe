7efe8ea863713316809a2720efc2e51f79ca6abf
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-02-13 15:18:40 +0530

    fixed

--- shoe_spectra/shoe_spectra/settings.py
+++ shoe_spectra/shoe_spectra/settings.py
@@ -200,7 +200,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
