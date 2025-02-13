ddd36d5d2090eae6b79918c8576244801ebf5d31
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-02-13 09:49:11 +0530

    static added

--- shoe_spectra/shoe_spectra/settings.py
+++ shoe_spectra/shoe_spectra/settings.py
@@ -199,7 +199,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 from django.contrib.messages import constants as messages
 
